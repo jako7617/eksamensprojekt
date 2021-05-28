09635c7e50e31bbda3f9312b75e86ab8f976bca8
Jakob Larsen <jakoblarsen10000@gmail.com>
2021-05-28 14:16:48 +0200

    pensumJS

---
+++ Pensum/pensum.js
@@ -0,0 +1,19 @@
+// ------ Jakob
+
+// ------ Jakob
+
+// ------ Jakob
+
+// ------ Jakob
+
+// Objekt
+const pensumsObjektJakob = {
+    Satellitter: "QuadSAT",
+    Drone: "Dyrt",
+    Projekt: "Okay",
+  };
+  console.log(pensumsObjektJakob);
+
+  pensumObjektJakob.javaScriptLavet = "ja";
+
+  console.log(pensumObjektJakob);
